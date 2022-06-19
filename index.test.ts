60f4dca9abbb8075ce264ef2f5e974f2daa3756c
itomise <itomise.mizumi@gmail.com>
2022-06-19 17:19:40 +0900

    add test

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,5 @@
 const sampleFunc = (bool: boolean) => bool
+const sampleNumberFunc = (number: number) => number
 
 describe('test func', () => {
     it('true should be true', () => {
@@ -6,5 +7,9 @@ describe('test func', () => {
     })
     it('false should be false', () => {
         expect(sampleFunc(false)).toBe(false)
+    })
+
+    it('1 should be 1', () => {
+        expect(sampleNumberFunc(1)).toBe(1)
     })
 })
